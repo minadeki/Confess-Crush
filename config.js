81498e16dd59b75aaf371700106f7104b7be5f6a
minadeki <94798517+minadeki@users.noreply.github.com>
2021-11-22 00:00:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Anh iuuuuu",
+    introTitle: 'Anh iuuuu TVT cua min',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    btnIntro: 'hay qué hay qué',
+    title: 'Phải chăng anh iu đã yêu em ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng anh iu đã say em ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Em iu thuỳ mị nết na giỏi giang lại còn xink đẹp thì ai mà không yêu cơ trứu',
+    btnNo: 'Mơ à, ai bảo iu cơ',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao anh iu lại yêu em thế nhờooo',
+    btnReply: 'Gửi ik gửi ik nào bùn ngủ qué rùi',
+    reply: 'Iu hok cần lý doooooo',
+    mess: 'Nma ý, NH iu TTr nhìu hơn cơ ý, iu nhắm nên là mỗi ngày, sao lại ko trao những lời iu thương cho nhữn ngừi mìn iu được nhỉ, hum nai NH sẽ iu TTr nhìu hơn hum qua mụt chút và như thế cứ nhân nhân tìn iu của mìn lên thui, iu TTr nhiều nhắm cơ 😘😘',
+    messDesc: 'Khi nãy mìn bùn cừi lắm =)) tại mìn ko biết hôn đến bao giờ mới xong ý =)) nma hok xao mìn bù mấy buổi sáng nhìu người cũn đc =)))) Đọc xong rùi thì còn chần chừ gì nữa mà ko thơm (online) tớ một cái nhỉ =)))))',
+    btnAccept: 'Iu NH nhìu hơn cơooo',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
